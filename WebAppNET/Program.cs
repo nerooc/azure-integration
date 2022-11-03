486558e41ef0674602c6b98718422eb0eb2d08b4
nerooc <tomasz_gajda@outlook.com>
2022-11-03 09:19:13 -0700

    testing

--- WebAppNET/Program.cs
+++ WebAppNET/Program.cs
@@ -1,8 +1,6 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-var configuration = (IConfiguration)app.Services.GetService(typeof(IConfiguration))!;
-
-app.MapGet("/", () => $"Hello CI/CD! Value: {configuration.GetSection("test").Value}");
+app.MapGet("/", () => "FAILING TEST!");
 
 app.Run();
