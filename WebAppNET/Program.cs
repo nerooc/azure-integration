47e63a436cf3c640987afbd89424318d6d007bf9
nerooc <tomasz_gajda@outlook.com>
2022-11-03 09:14:29 -0700

    add configuration

--- WebAppNET/Program.cs
+++ WebAppNET/Program.cs
@@ -1,6 +1,8 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello CI/CD!");
+var configuration = (IConfiguration)app.Services.GetService(typeof(IConfiguration))!;
+
+app.MapGet("/", () => $"Hello CI/CD! Value: {configuration.GetSection("test").Value}");
 
 app.Run();
